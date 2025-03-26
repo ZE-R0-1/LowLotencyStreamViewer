52bfb803a85613ce7eff609138a839190ca858c9
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2025-03-26 09:15:48 +0900

    Initial commit

---
+++ LowLotencyStreamViewer/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LowLotencyStreamViewer
+//
+//  Created by USER on 3/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+}
